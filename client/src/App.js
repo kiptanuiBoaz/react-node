1887eab0a95f78bcc7e4a99e5a7e35e331a82703
Boaz <boaserem022@gmail.com>
2022-09-17 10:47:04 +0300

    client and  api connect success

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 
-class App extends React.Component {
-  constructor(props) {
-    uper(props);
-    this.state ={apiResponse:""}
-  }
-
-  callApi(){
-    fetch("https://localhost:9000/testAPI")
-      .then((res=> res.text()))
-      .then((res=> res.setState({apiResponse:res})))
-  }
-
-  componentWillMount() {
-    this.callApi()
-}
-
-
-render() {
+export const App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <p>{this.state.apiResponse}</p>
-        </a>
-      </header>
+    <div >
+      This is the client side
+
+      <form action="../../post" method="post" 
+              className="form">
+          <button type="submit">Connected?</button>
+        </form>
     </div>
   );
 }
 
-export default App;
+
